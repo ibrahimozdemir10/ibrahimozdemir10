643da610a427e9e8e3a48bd0f9815ab4027fe66e
ibrahimozdemir10 <114670437+ibrahimozdemir10@users.noreply.github.com>
2022-10-03 15:56:37 +0300

    Add files via upload

---
+++ odevim.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+
+</head>
+<body>
+
+<?php
+
+
+$sayi1 = 29;
+$sayi2 = 15;
+
+echo " Toplama İslemi Sonucu : " .($sayi1 + $sayi2 )."<br />" ;
+echo " Cıkarma İslemi Sonucu : " .($sayi1 - $sayi2 )."<br />";
+echo " Bolme İslemi Sonucu : " .($sayi1  / $sayi2 )."<br />" ;
+echo " Carpma İslemi Sonucu : " .($sayi1 * $sayi2 )."<br />";
+
+
+?>
+</body>
+</html>
